e62184c976994454b90b243b533d7cb6f92213ee
Raymond <raymond@philippinedev.com>
2021-08-18 13:44:07 +0800

    add app models and associations

---
+++ db/schema.rb
@@ -0,0 +1,24 @@
+ActiveRecord::Schema.define(version: 2021_08_18_022519) do
+
+  enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.integer "physician_id"
+    t.integer "patient_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "physicians", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
